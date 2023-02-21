242cfc9cc20bbe1ec92d241523a5de044a2cab0b
adamsartur <adamsartur@gmail.com>
2023-02-21 16:38:56 -0300

    commenting stuff for test

--- src/App.tsx
+++ src/App.tsx
@@ -40,50 +40,53 @@ export function App() {
   }, [loadPosts]);
 
   return (
-    <div className="page-wrapper">
-      {isLoading && (
-        <div className="overlay-loader">
-          <span>
-            <Spinner size={32} />
-          </span>
-        </div>
-      )}
-      <div className="posts-wrapper">
-        <h1>
-          <QRModal isQRVisible={isQRVisible} setIsQRVisible={setIsQRVisible} />
-          <QRReaderButton
-            isQRVisible={isQRVisible}
-            setIsQRVisible={setIsQRVisible}
-          />
-          Post list{" "}
-          <a
-            title="Create a new entry"
-            className="create-post-button"
-            onClick={() => {
-              setIsNewPostOpen(true);
-            }}
-            disabled={isLoading}
-          >
-            <Plus size={24} />
-          </a>
-        </h1>
-        <ul>
-          {posts.map((post: Post) => (
-            <Post
-              post={post}
-              setLoadPosts={setLoadPosts}
-              setIsLoading={setIsLoading}
-            />
-          ))}
-        </ul>
-      </div>
-      <CreatePost
-        setLoadPosts={setLoadPosts}
-        setIsNewPostOpen={setIsNewPostOpen}
-        isNewPostOpen={isNewPostOpen}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-      />
+    <div>
+      hello world
     </div>
+    // <div className="page-wrapper">
+    //   {isLoading && (
+    //     <div className="overlay-loader">
+    //       <span>
+    //         <Spinner size={32} />
+    //       </span>
+    //     </div>
+    //   )}
+    //   <div className="posts-wrapper">
+    //     <h1>
+    //       <QRModal isQRVisible={isQRVisible} setIsQRVisible={setIsQRVisible} />
+    //       <QRReaderButton
+    //         isQRVisible={isQRVisible}
+    //         setIsQRVisible={setIsQRVisible}
+    //       />
+    //       Post list{" "}
+    //       <a
+    //         title="Create a new entry"
+    //         className="create-post-button"
+    //         onClick={() => {
+    //           setIsNewPostOpen(true);
+    //         }}
+    //         disabled={isLoading}
+    //       >
+    //         <Plus size={24} />
+    //       </a>
+    //     </h1>
+    //     <ul>
+    //       {posts.map((post: Post) => (
+    //         <Post
+    //           post={post}
+    //           setLoadPosts={setLoadPosts}
+    //           setIsLoading={setIsLoading}
+    //         />
+    //       ))}
+    //     </ul>
+    //   </div>
+    //   <CreatePost
+    //     setLoadPosts={setLoadPosts}
+    //     setIsNewPostOpen={setIsNewPostOpen}
+    //     isNewPostOpen={isNewPostOpen}
+    //     isLoading={isLoading}
+    //     setIsLoading={setIsLoading}
+    //   />
+    // </div>
   );
 }
