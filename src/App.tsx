dd0cc942570e508ed7dca3aabd2276ef22a6d0f4
Artur Adams <adamsartur@gmail.com>
2023-02-26 00:34:12 -0300

    mindmap api

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
-import { Trash, Spinner, Plus } from "phosphor-react";
-import { useEffect, useState } from "preact/hooks";
-import CreatePost from "./components/CreatePost";
-import Post from "./components/Post";
-import QRModal from "./components/QRModal";
-import QRReaderButton from "./components/QRReaderButton";
-
-type Post = {
-  id: number;
-  title: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
-  authorId: number;
-  published: boolean;
-};
+import { Spinner } from "phosphor-react";
+import { useState } from "preact/hooks";
+import Posts from "./components/Posts";
 
 export function App() {
-  const [loadPosts, setLoadPosts] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [isNewPostOpen, setIsNewPostOpen] = useState(false);
-  const [posts, setPosts] = useState([]);
   const [isQRVisible, setIsQRVisible] = useState(false);
-
-  useEffect(() => {
-    async function load() {
-      if (!loadPosts) {
-        return;
-      }
-      setIsLoading(true);
-      const allPosts = await fetch("/.netlify/functions/posts").then((res) =>
-        res.json()
-      );
-      console.log("posts retrieved");
-      setPosts(allPosts);
-      setLoadPosts(false);
-    }
-    setIsLoading(false);
-    load();
-  }, [loadPosts]);
 
   return (
     <div className="page-wrapper">
@@ -48,41 +16,13 @@ export function App() {
           </span>
         </div>
       )}
-      <div className="posts-wrapper">
-        <h1>
-          <QRModal isQRVisible={isQRVisible} setIsQRVisible={setIsQRVisible} />
-          <QRReaderButton
-            isQRVisible={isQRVisible}
-            setIsQRVisible={setIsQRVisible}
-          />
-          Post list{" "}
-          <a
-            title="Create a new entry"
-            className="create-post-button"
-            onClick={() => {
-              setIsNewPostOpen(true);
-            }}
-            disabled={isLoading}
-          >
-            <Plus size={24} />
-          </a>
-        </h1>
-        <ul>
-          {posts.map((post: Post) => (
-            <Post
-              post={post}
-              setLoadPosts={setLoadPosts}
-              setIsLoading={setIsLoading}
-            />
-          ))}
-        </ul>
-      </div>
-      <CreatePost
-        setLoadPosts={setLoadPosts}
-        setIsNewPostOpen={setIsNewPostOpen}
-        isNewPostOpen={isNewPostOpen}
+      <Posts
         isLoading={isLoading}
+        isNewPostOpen={isNewPostOpen}
+        isQRVisible={isQRVisible}
         setIsLoading={setIsLoading}
+        setIsNewPostOpen={setIsNewPostOpen}
+        setIsQRVisible={setIsQRVisible}
       />
     </div>
   );
