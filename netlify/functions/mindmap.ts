deb488ac42c5dca9ffef60a1c35f2632c18db5cc
Artur Adams <adamsartur@gmail.com>
2023-02-26 20:19:50 -0300

    api for mindmaps

--- netlify/functions/mindmap.ts
+++ netlify/functions/mindmap.ts
@@ -2,67 +2,51 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 interface ManageMindMap {
-  operation: "create" | "update" | "delete";
-  mindMapId: string;
+  mindMapId?: string;
   name: string;
   nodes?: any[];
   edges?: any[];
 }
 
 export async function handler(event) {
-  const { operation, mindMapId, nodes, edges, name } = JSON.parse(
-    event.body
-  ) as ManageMindMap;
+  const {
+    mindMapId,
+    nodes = [],
+    edges = [],
+    name,
+  } = JSON.parse(event.body) as ManageMindMap;
+
+  console.log(edges);
 
   let result;
   const mindMapData: any = {
-    id: mindMapId,
+    name: name,
   };
+  if (nodes && nodes.length > 0) {
+    mindMapData.nodes = nodes;
+  }
+  if (edges && edges.length > 0) {
+    mindMapData.edges = edges;
+  }
 
-  if (name) {
-    mindMapData.name = name;
-  }
-  switch (operation) {
-    case "create":
-      if (nodes && nodes.length > 0) {
-        mindMapData.nodes = { create: nodes };
-      }
-      if (edges && edges.length > 0) {
-        mindMapData.edges = { create: edges };
-      }
-      result = await prisma.mindMaps.create({ data: mindMapData });
-      break;
-    case "update":
-      if (nodes && nodes.length > 0) {
-        mindMapData.nodes = { set: nodes };
-      }
-      if (edges && edges.length > 0) {
-        mindMapData.edges = { set: edges };
-      }
-      result = await prisma.mindMaps.update({
-        where: { id: mindMapId },
-        data: mindMapData,
-      });
-      break;
-    case "delete":
-      result = await prisma.mindMaps.delete({
-        where: { id: mindMapId },
-      });
-      break;
-    default:
-      return {
-        statusCode: 400,
-        header: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          message: `Unsupported operation: ${operation}`,
-        }),
-      };
-  }
+  console.log(mindMapData);
+  result = await prisma.mindMaps.create({
+    data: mindMapData,
+  });
+
+  await prisma.mindMaps.delete({
+    where: {
+      id: mindMapId,
+    },
+  });
 
   return {
     statusCode: 200,
     header: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     },
     body: JSON.stringify(result),
   };
--- netlify/functions/mindmaps.ts
+++ netlify/functions/mindmaps.ts
@@ -6,20 +6,17 @@ export async function handler(event) {
     ? JSON.parse(event.body)
     : { id: "d25194b8-7bd5-4988-807f-86a244f3afde" };
 
-  const mindmaps = await prisma.mindMaps.findMany({
-    select: {
-      nodes: true,
-      edges: true,
-    },
-    where: {
-      id: id,
-    },
-  });
+  let response;
+
+  response = await prisma.mindMaps.findFirst();
   return {
     statusCode: 200,
     header: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     },
-    body: JSON.stringify(mindmaps),
+    body: JSON.stringify(response),
   };
 }
